a46788300410e0824063aaef96e9216a2662f45a
Joshua Madden <jmaddenco@gmail.com>
2015-10-07 09:58:31 -0600

    Create main window

---
+++ main.rb
@@ -0,0 +1,24 @@
+require "gosu"
+
+class GameWindow < Gosu::Window
+	def initialize
+		super 640, 480
+		self.caption = "Gosu Tutorial Game"
+	end
+
+
+	def update
+
+	end
+
+	def draw
+		
+	end
+
+	def button_down id
+		close if id == Gosu::KbEscape
+	end
+end
+
+window = GameWindow.new
+window.show
